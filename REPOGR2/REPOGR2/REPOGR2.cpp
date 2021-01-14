fa7a7ef52a78dd2574046cfcbc3ef6e58bd7c8b3
Mikołaj Kusiński <mikolajk558@gmail.com>
2021-01-14 13:13:29 +0100

    Czy jest ok?

--- REPOGR2/REPOGR2/REPOGR2.cpp
+++ REPOGR2/REPOGR2/REPOGR2.cpp
@@ -1,8 +1,22 @@
-﻿
-#include <iostream>
+﻿#include <iostream>
+#include<cmath>
 using namespace std;
-
+double trojkat(double a, double b, double c);
 int main()
 {
- cout << "Hello World!\n";
+	double a, b, c;
+	cout << "Podaj dlugosc boku a : "; cin >> a;
+	cout << "Podaj dlugosc boku b : "; cin >> b;
+	cout << "podaj dlugosc boku c : "; cin >> c;
+	cout << "Pole trojkata o bokach: " << a << " " << b << " " << c << " " << "wynosi : " << trojkat(a, b, c);
+}
+double trojkat(double a, double b, double c)
+{
+	int p = (a + b + c) / 2;
+	if ((a + c > b) && (b + c > a) && (a + b > c))
+	{
+		return sqrt(p * (p - a) * (p - b) * (p - c));
+	}
+	else
+		return -1;
 }
